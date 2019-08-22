f0f1653cbe384256c24155ca324ddc31a95dea8f
Bas Doppen <basdoppen@gmail.com>
2019-08-22 14:14:56 +0200

    temp markup

--- _dist/wp-huc-theme/template-parts/outputList-presentation.php
+++ _dist/wp-huc-theme/template-parts/outputList-presentation.php
@@ -1,62 +1,11 @@
 <div class="">
 
   <div>
-    <em><?php the_title(); ?></em> <span class="hcTxtColorGreyMid pub_add"><?php echo get_post_meta($post->ID, 'additional_notes', true); ?></span>
+    <strong><em><?php the_title(); ?></em></strong> <span class="pub_add"><?php echo get_post_meta($post->ID, 'additional_notes', true); ?></span>
 
   </div>
-  <span class="hcSmallTxt">
-    <?php
-      if (date("d-m-Y", strtotime(get_post_meta($post->ID, 'publication_date', true))) != '01-01-1970') {
-        echo 'Published on '.date("d-m-Y", strtotime(get_post_meta($post->ID, 'publication_date', true)));
-      }
 
-     ?>
-    by
-    <?php
-    $pod = pods( 'output', get_the_id() );
-    $related = $pod->field( 'creators' );
-
-
-    if ( ! empty( $related ) ) {
-      foreach ( $related as $rel ) {
-        $id = $rel[ 'ID' ];
-        $authors =$authors.get_the_title( $id ).', ';
-
-      }
-      $authors = substr_replace($authors, "", -2);
-      echo $authors;
-
-      $creat2 = get_post_meta($post->ID, 'creator_2', true);
-      $creat2Name =  $creat2['post_title'];
-      if ($creat2Name != '') {
-        echo ', '.$creat2Name;
-      }
-
-      $creat3 = get_post_meta($post->ID, 'creator_3', true);
-      $creat3Name =  $creat3['post_title'];
-      if ($creat3Name != '') {
-        echo ', '.$creat3Name;
-      }
-
-      $creat4 = get_post_meta($post->ID, 'creator_4', true);
-      $creat4Name =  $creat4['post_title'];
-      if ($creat4Name != '') {
-        echo ', '.$creat4Name;
-      }
-
-      $creat5 = get_post_meta($post->ID, 'creator_5', true);
-      $creat5Name =  $creat5['post_title'];
-      if ($creat5Name != '') {
-        echo ', '.$creat5Name;
-      }
-
-
-
-
-
-    } ?>
-    </span>
-    <div class="">
+    <div class="hcTxtColorGreyMid">
       <?php if (get_post_meta($post->ID, 'filelink', true) !='' ) {
               $linkId = get_post_meta($post->ID, 'filelink', false);
               $link_url = wp_get_attachment_url($linkId[0][ID]);
@@ -65,7 +14,8 @@
               echo 'Download: '.$link_url.'';
       }
        ?>
-       </div><div class="">
+       </div>
+       <div class="hcTxtColorGreyMid">
        <?php if (get_post_meta($post->ID, 'linktosite', true) !='' ) {
                $link_url = get_post_meta($post->ID, 'linktosite', true);
 
@@ -74,5 +24,55 @@
        }
         ?>
     </div>
+
+
+    <span class="hcSmallTxt">
+      <?php
+        if (date("d-m-Y", strtotime(get_post_meta($post->ID, 'publication_date', true))) != '01-01-1970') {
+          echo 'Published on '.date("d-m-Y", strtotime(get_post_meta($post->ID, 'publication_date', true)));
+        }
+
+       ?>
+      by
+      <?php
+      $pod = pods( 'output', get_the_id() );
+      $related = $pod->field( 'creators' );
+
+
+      if ( ! empty( $related ) ) {
+        foreach ( $related as $rel ) {
+          $id = $rel[ 'ID' ];
+          $authors =$authors.get_the_title( $id ).', ';
+
+        }
+        $authors = substr_replace($authors, "", -2);
+        echo $authors;
+
+        $creat2 = get_post_meta($post->ID, 'creator_2', true);
+        $creat2Name =  $creat2['post_title'];
+        if ($creat2Name != '') {
+          echo ', '.$creat2Name;
+        }
+
+        $creat3 = get_post_meta($post->ID, 'creator_3', true);
+        $creat3Name =  $creat3['post_title'];
+        if ($creat3Name != '') {
+          echo ', '.$creat3Name;
+        }
+
+        $creat4 = get_post_meta($post->ID, 'creator_4', true);
+        $creat4Name =  $creat4['post_title'];
+        if ($creat4Name != '') {
+          echo ', '.$creat4Name;
+        }
+
+        $creat5 = get_post_meta($post->ID, 'creator_5', true);
+        $creat5Name =  $creat5['post_title'];
+        if ($creat5Name != '') {
+          echo ', '.$creat5Name;
+        }
+
+      } ?>
+      </span>
 
 </div>
