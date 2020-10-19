aa74ca586c9ca1f1317181a3fdfa1b70a2bd89dc
Bas Doppen <basdoppen@gmail.com>
2020-10-19 22:13:02 +0200

    post page

---
+++ src/wp/template_posts-list.php
@@ -0,0 +1,50 @@
+<?php /* Template Name: List allPosts in a list  */ ?>
+<?php
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+	<?php
+	// page
+	while ( have_posts() ) : the_post();
+
+		get_template_part( 'template-parts/content', 'page' );
+
+	endwhile; // End of the loop.
+
+
+
+	// posts
+	wp_reset_postdata();
+
+
+  $args = array(
+      'order'     => 'DESC',
+      'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field' => 'slug',
+                'operator' => 'NOT IN'
+                )
+            )
+  );
+  query_posts( $args );
+
+  if ( have_posts() ) :
+
+    ?>
+    <div class="itemBlock">
+    <?php
+
+    while ( have_posts() ) : the_post();
+      get_template_part( 'template-parts/post-news' );
+    endwhile;
+  endif;
+?>
+
+</main><!-- #primary -->
+
+<?php
+
+get_footer();
