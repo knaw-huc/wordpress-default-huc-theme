8f6a540cc9b6fb588c45102d3b26c094fe8c1ec6
BD <basdoppen@gmail.com>
2021-04-08 16:10:52 +0200

    caption on home items

--- src/wp/template-parts/homepage-item.php
+++ src/wp/template-parts/homepage-item.php
@@ -7,6 +7,10 @@
     <div class="homeItemImg">
     <?php if ( has_post_thumbnail() ) {?>
       <img src="<?php the_post_thumbnail_url('medium') ?>">
+      <div class="smallGrey">
+        <?php $image_id = get_post_thumbnail_id(get_the_ID());
+        echo get_post_meta($image_id , '_wp_attachment_image_alt', true); ?>
+      </div>
     <?php } ?>
     </div>
   </div>
